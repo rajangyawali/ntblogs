ac33fcb3313642096747d8682d433f5cc366a4bf
Rajan Gyawali <rajangyawali93@gmail.com>
2020-06-01 14:17:13 +0545

    Initial Commit

--- NTBlogs/settings.py
+++ NTBlogs/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
     }
 }
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
