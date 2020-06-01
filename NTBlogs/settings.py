2a453c0487d6573470194095cdfb0b99ab8e4772
Rajan Gyawali <rajangyawali93@gmail.com>
2020-06-01 14:55:23 +0545

    Initial Commit

--- NTBlogs/settings.py
+++ NTBlogs/settings.py
@@ -163,4 +163,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
