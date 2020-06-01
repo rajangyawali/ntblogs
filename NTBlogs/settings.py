fc6c57852037341d642e473d148034ea3a15da8f
Rajan Gyawali <rajangyawali93@gmail.com>
2020-06-01 11:04:02 +0545

    Initial Commit

--- NTBlogs/settings.py
+++ NTBlogs/settings.py
@@ -155,6 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_cdn')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATICFILES_DIR,]
 
