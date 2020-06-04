1b0b17c215248a752b3a00b0e1814be4445dea0a
Rajan Gyawali <rajangyawali93@gmail.com>
2020-06-04 12:52:21 +0545

    Final Commit

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from . import models
 
-from . models import BlogPost
-
 class ContactForm(forms.Form):
     email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
@@ -11,15 +9,4 @@ class ContactForm(forms.Form):
 class SubscriberForm(forms.Form):
     subscriber = forms.EmailField(required=True)
 
-# class BlogPostForm(forms.Form):
-#     class Meta:
-#         model = BlogPost
-#         fields = ['title', 'description', 'featured', 'image', 'category', 'author']
-    
-#     title = forms.CharField(required=True)
-#     description = forms.CharField(widget=forms.Textarea, required=True)
-#     featured = forms.ChoiceField(required=True, choices=models.FEATURED_OPTIONS, de)
-#     image = forms.ImageField(required=True)
-#     category = forms.ChoiceField(required=True, choices=models.CATEGORY_OPTIONS)
-#     author = forms.ChoiceField(required=True)
-    
+   
